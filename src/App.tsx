f3921efa5067b5c92a6fdd545fa5381fd495de08
Farghali <iamfarghali@gmail.com>
2026-05-05 09:37:18 +0300

    feat(products): add search filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import type { Product } from '@/types/product';
 import ProductCard from './components/ProductCard';
+import { useState } from 'react';
 
 const products: Product[] = [
   { id: 1, name: 'Laptop', price: 1200 },
@@ -12,10 +13,29 @@ const products: Product[] = [
   { id: 8, name: 'Smartwatch', price: 250 },
 ];
 function App() {
+  const [search, setSearch] = useState('');
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="container h-screen mx-auto px-10 pt-12 bg-gray-50">
+      {/* Search */}
+      <div className="border mb-4">
+        <input
+          type="text"
+          name="product_name"
+          value={search}
+          className="w-full px-2"
+          placeholder="Search..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
+      {/* Products */}
       <div className="grid grid-cols-4 gap-4">
-        {products.map((p) => (
+        {filteredProducts.map((p) => (
           <ProductCard product={p} />
         ))}
       </div>
